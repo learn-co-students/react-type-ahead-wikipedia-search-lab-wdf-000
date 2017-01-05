3e333a8fb9b24e264b62a08490fd40d15aa41311
Isuru <github email address>
2017-01-05 15:53:48 -0500

    Done.

--- components/SearchField.js
+++ components/SearchField.js
@@ -2,8 +2,16 @@
 
 const React = require('react');
 
-const SearchField = props => (
-  <input />
-);
+// const SearchField = props => (
+//   <input className='search-field' value={props.value} onChange={props.onChange}/>
+// );
+
+class SearchField extends React.Component{
+  render(){
+    return(
+      <input className='search-field' value={this.props.value} onChange={this.props.onChange}/>
+    )
+  }
+}
 
 module.exports = SearchField;
