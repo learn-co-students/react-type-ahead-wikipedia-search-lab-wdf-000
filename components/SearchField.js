e314b119410a1ce83f07848063c3834dd52c7bf6
Wendy Xu <guangw.xu@gmail.com>
2017-02-04 23:31:30 -0500

    Done.

--- components/SearchField.js
+++ components/SearchField.js
@@ -3,7 +3,7 @@
 const React = require('react');
 
 const SearchField = props => (
-  <input />
+  <input className='search-field' value={props.value} onChange={props.onChange} />
 );
 
 module.exports = SearchField;
--- stores/resultStore.js
+++ stores/resultStore.js
@@ -2,7 +2,13 @@
 
 const Store = require('./Store');
 
-class ResultStore {
+class ResultStore extends Store {
+  isOutdated(updated) {
+    return this.getState().updated > updated;
+  }
 }
 
-module.exports = new ResultStore();
+module.exports = new ResultStore({
+  results: [],
+  updated: new Date()
+});
