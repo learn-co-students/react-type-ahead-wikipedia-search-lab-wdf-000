625cefbb0926adc22f3d27d1356366f864719564
parm530 <parmsahadeo530@gmail.com>
2017-02-07 17:13:42 -0500

    Done

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -3,8 +3,17 @@
 const React = require('react');
 
 const SearchResults = ({ results }) => (
-  <ul>
+  <ul className="search-results">
+    {
+      results.map((result, i) => (
+        <li >
+          <p>{result.description}</p>
+          <a href={result.link}>{result.title}</a>
+        </li>
+      ))
+    }
   </ul>
 );
+
 
 module.exports = SearchResults;
--- stores/Store.js
+++ stores/Store.js
@@ -7,7 +7,12 @@ class Store {
   }
 
   addListener(listener) {
-    // TODO
+    this.listeners.push(listener);
+    const removeListener = () => {
+      this.listeners = this.listeners.filter((l) => listener !== l);
+    };
+
+    return removeListener;
   }
 
   setState(state) {
